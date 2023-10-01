4c02d32930ddaa0119dc4a418cb795705c402602
Piotr Rosiński <piotr.rosinski18@gmail.com>
2023-10-01 22:37:13 +0200

    led&buzzer code added to reed switch

--- Reed_Switch_Test/Reed_Switch_Test.ino
+++ Reed_Switch_Test/Reed_Switch_Test.ino
@@ -1,22 +1,30 @@
-const int REED_PIN = D2;	// pin connected to reed switch
+#define sensor D1			//pin, do którego dołączony jest czujnik
+#define buzzer D5   //pin, do którego dołączony jest brzęczyk
+#define led D8			//pin, do którego dołączony jest LED
 
 void setup() {
-	Serial.begin(9600);
-	pinMode(REED_PIN, INPUT_PULLUP);	// enable internal pull-up for the reed switch
+	Serial.begin(9600);				//otwórz transmisję szeregową
+	pinMode(sensor, INPUT_PULLUP);	//włącz wewnętrzny pull-up dla kontaktronu
+	pinMode(led, OUTPUT);      		//ustaw LED jako wyjście 
+	pinMode(buzzer, OUTPUT);		//ustaw brzęczyk jako wyjście
+	Serial.begin(9600); 			//otwórz transmisję szeregową
 }
 
 void loop() {
-  if (digitalRead(REED_PIN) == HIGH) 
-  {
-    Serial.println("Door opened.");
+	if (digitalRead(sensor) == HIGH) {
+    Serial.println("Otworzono drzwi/okno.");
     Serial.println("");
-    delay(100); 						// 100 ms delay
+	digitalWrite(led, HIGH);   			//włącz LED
+    digitalWrite(buzzer, HIGH); 		//włącz brzęczyk
+    delay(1000); 					
   }
 
 	else 
 	{
-	Serial.println("Door closed.");
-    Serial.println("");
-    delay(100);
+		Serial.println("Zamknięto drzwi/okno.");
+		Serial.println("");
+		digitalWrite(led, LOW);   		//wyłącz LED
+		digitalWrite(buzzer, LOW); 		//wyłącz brzęczyk
+		delay(1000);
 	}
 }
