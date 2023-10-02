ea0c3bc252c52aeda7e9653a74127c9d9832eaf3
Piotr Rosiński <piotr.rosinski18@gmail.com>
2023-10-02 12:34:33 +0200

    led&buzzer added to CO2 sensor, defined safe levels CO2

--- Gas_Sensor/Gas_Sensor.ino
+++ Gas_Sensor/Gas_Sensor.ino
@@ -16,13 +16,24 @@ void loop() {
     Serial.println("Ilość CO2 (w PPM): ");
     Serial.println(value);
 	
-	if (value > 15000) {				  //jeżeli wartość większa niż 500 PPM CO2
+	if (value > 15000) {				  //jeżeli wartość większa niż 15000 PPM CO2
+  //Maksymalne stężenie w specyficznych warunkach pracy np. łodzie podwodne
+    Serial.println("Przekroczono próg 15000 PPM CO2");
+		digitalWrite(led, HIGH);   	  //włącz LED
+		digitalWrite(buzzer, HIGH);   //włącz brzęczyk
+		delay(100);                   //opóźnij 100 ms
+	}
+
+  else if (value > 1000) {				  //jeżeli wartość większa niż 1000 PPM CO2
+  //Górny próg w pomieszczeniach stałego przebywania ludzi według WHO
+    Serial.println("Przekroczono próg 1000 PPM CO2");
 		digitalWrite(led, HIGH);   	  //włącz LED
 		digitalWrite(buzzer, HIGH);   //włącz brzęczyk
 		delay(100);                   //opóźnij 100 ms
 	}
 	
 	else {
+    Serial.println("Stężenie CO2 OK");
 		digitalWrite(led, LOW);   	  	//wyłącz LED
 		digitalWrite(buzzer, LOW);   	//wyłącz brzęczyk
 		delay(100);           			//opóźnij 100 ms
