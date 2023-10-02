ec11cae29957711b73ea882adad6bf248250ba42
Piotr Rosiński <piotr.rosinski18@gmail.com>
2023-10-02 14:24:13 +0200

    online toast on start-up added

--- Security_System_App/app/src/main/java/com/example/securitysystemapp/MainActivity.java
+++ Security_System_App/app/src/main/java/com/example/securitysystemapp/MainActivity.java
@@ -2,8 +2,14 @@ package com.example.securitysystemapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.widget.Toast;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +20,20 @@ public class MainActivity extends AppCompatActivity {
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN); //Fullscreen mode (without toolbar on the top)
+
+        if (isOnline()) {
+            Toast.makeText(MainActivity.this.getApplicationContext(),
+                    "Connection found.", Toast.LENGTH_SHORT).show();
+
+        } else {
+            Toast.makeText(MainActivity.this.getApplicationContext(),
+                    "No connection found. Connect your device and try again later.", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public boolean isOnline() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        return (networkInfo != null && networkInfo.isConnected());
     }
 }
