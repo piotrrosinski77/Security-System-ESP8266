301d9d3735e1da4872d56d191345d8f2f617d334
Piotr Rosiński <piotr.rosinski18@gmail.com>
2023-12-12 17:25:01 +0100

    added POST method

--- Water_Sensor/Water_Sensor.ino
+++ Water_Sensor/Water_Sensor.ino
@@ -1,17 +1,40 @@
-#define sensor A0             //pin, do którego dołączony jest czujnik
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+
+const char* ssid = "NETIASPOT-2.4GHz-5D9Q";
+const char* password = "eszewerie95";
+
+unsigned long lastTime = 0;
+unsigned long timerDelay = 5000;
+
+#define sensor A0       //pin, do którego dołączony jest czujnik
 #define power D1 				//pin zasilający czujnik 
-#define buzzer D5             //pin, do którego dołączony jest brzęczyk
-#define led D8				//pin, do którego dołączony jest LED
+#define buzzer D5       //pin, do którego dołączony jest brzęczyk
+#define led D8				  //pin, do którego dołączony jest LED
 
 int value = 0; 					//wartość początkowa stanu czujnika
 
 void setup() {
-	pinMode(sensor, INPUT);		//ustaw czujnik jako wejście
+	pinMode(sensor, INPUT);		  //ustaw czujnik jako wejście
 	pinMode(power, OUTPUT);   	//ustaw pin zasilający jako wyjście
 	pinMode(led, OUTPUT);      	//ustaw LED jako wyjście 
-	pinMode(buzzer, OUTPUT);	//ustaw brzęczyk jako wyjście
+	pinMode(buzzer, OUTPUT);	  //ustaw brzęczyk jako wyjście
 	digitalWrite(power, LOW); 	//wyłącz czujnik
-	Serial.begin(9600);			//otwórz transmisję szeregową
+	Serial.begin(115200);
+
+	WiFi.begin(ssid, password);
+	Serial.println("Connecting");
+	
+	while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+	}
+	Serial.println("");
+	Serial.print("Connected to WiFi network with IP Address: ");
+	Serial.println(WiFi.localIP());
+ 
+	Serial.println("Timer set to 5 seconds (timerDelay variable), it will take 5 seconds before publishing the first reading.");
 }
 
 void loop() {
@@ -21,20 +44,47 @@ void loop() {
 	digitalWrite(power, LOW);     //wyłącz czujnik
 	
 	Serial.print("Ilość wody: "); //wypisz w transmisji szeregowej
-	Serial.println(value);		  //wypisz wartość czujnika
-	Serial.println("");			  //wypisz linijkę odstępu
+	Serial.println(value);		    //wypisz wartość czujnika
+	Serial.println("");			      //wypisz linijkę odstępu
 	
-	if (value > 400) {				  //jeżeli wartość większa niż 400 jednostek wody
-		digitalWrite(led, HIGH);   	  //włącz LED
-		digitalWrite(buzzer, HIGH);   //włącz brzęczyk
-		delay(100);                   //opóźnij 100 ms
+	if (value > 400) {				    //jeżeli wartość większa niż 400 jednostek wody
+		digitalWrite(led, HIGH);   	//włącz LED
+		digitalWrite(buzzer, HIGH); //włącz brzęczyk
+		delay(100);                 //opóźnij 100 ms
 	}
 	
 	else {
-		digitalWrite(led, LOW);   	  	//wyłącz LED
-		digitalWrite(buzzer, LOW);   	//wyłącz brzęczyk
+		digitalWrite(led, LOW);   	//wyłącz LED
+		digitalWrite(buzzer, LOW);  //wyłącz brzęczyk
 		delay(100);           			//opóźnij 100 ms
   }
 
-	delay(1000);				  		//opóźnij 1000 ms
+  delay(1000);
+	
+	if ((millis() - lastTime) > timerDelay) {
+    
+    if(WiFi.status()== WL_CONNECTED){
+
+      HTTPClient http;
+      WiFiClientSecure client;
+	    client.setInsecure();
+
+      http.begin(client,"https://security-system-api-260273149601.herokuapp.com/water");
+  
+      http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+      
+      String httpRequestData = "value=" + String(value);           
+      int httpResponseCode = http.POST(httpRequestData);
+      String payload = http.getString();
+      Serial.print("HTTP Response code: ");
+      Serial.println(httpResponseCode);
+      Serial.println(payload);
+    
+      http.end();
+    }
+    else {
+      Serial.println("WiFi Disconnected");
+    }
+    lastTime = millis();
+  }
 }
