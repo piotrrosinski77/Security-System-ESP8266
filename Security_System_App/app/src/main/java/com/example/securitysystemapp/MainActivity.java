8e49fbdad3ac588c84c9c5b2fe2d47342fd0d439
Piotr Rosiński <piotr.rosinski18@gmail.com>
2023-12-14 20:32:48 +0100

    added handlers

--- Security_System_App/app/src/main/java/com/example/securitysystemapp/MainActivity.java
+++ Security_System_App/app/src/main/java/com/example/securitysystemapp/MainActivity.java
@@ -1,31 +1,31 @@
 package com.example.securitysystemapp;
 import static kotlin.io.ConsoleKt.readLine;
-
 import java.io.IOException;
 import androidx.appcompat.app.AppCompatActivity;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.widget.TextView;
 import android.widget.Toast;
-import android.app.FragmentTransaction;
+import android.os.AsyncTask;
+import android.util.Log;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;import android.app.FragmentTransaction;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 import org.json.JSONException;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.ArrayList;
+import java.io.IOException;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,6 +33,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView status = findViewById(R.id.status);
+        TextView burIcon = findViewById(R.id.burIcon);
+        TextView coIcon = findViewById(R.id.coIcon);
+        TextView watIcon = findViewById(R.id.watIcon);
+        TextView soundIcon = findViewById(R.id.soundIcon);
+
+        //status.setText("Alarm");
+        //status.setText("OK");
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN); //Fullscreen mode (without status bar)
@@ -44,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
             LoadValues task = new LoadValues();
             task.execute();
         }
-            else {
+        else {
             Toast.makeText(MainActivity.this.getApplicationContext(),
                     "No connection found. Connect your device and try again later.", Toast.LENGTH_LONG).show();
         }
@@ -59,62 +68,152 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected Long doInBackground(String... params) {
+
             HttpURLConnection connWater = null;
             HttpURLConnection connSound = null;
+            HttpURLConnection connGas = null;
+            HttpURLConnection connMotion = null;
+            HttpURLConnection connReedSwitch = null;
 
             int statusWater;
             int statusSound;
+            int statusGas;
+            int statusMotion;
+            int statusReedSwitch;
+
             try {
+
                 URL dataUrlWater = new URL("https://security-system-api-260273149601.herokuapp.com/water");
                 URL dataUrlSound = new URL("https://security-system-api-260273149601.herokuapp.com/sound");
+                URL dataUrlGas = new URL("https://security-system-api-260273149601.herokuapp.com/gas");
+                URL dataUrlMotion = new URL("https://security-system-api-260273149601.herokuapp.com/motion");
+                URL dataUrlReedSwitch = new URL("https://security-system-api-260273149601.herokuapp.com/reedswitch");
+
                 connWater = (HttpURLConnection) dataUrlWater.openConnection();
                 connSound = (HttpURLConnection) dataUrlSound.openConnection();
+                connGas = (HttpURLConnection) dataUrlGas.openConnection();
+                connMotion = (HttpURLConnection) dataUrlMotion.openConnection();
+                connReedSwitch = (HttpURLConnection) dataUrlReedSwitch.openConnection();
+
                 connWater.connect();
                 connSound.connect();
+                connGas.connect();
+                connMotion.connect();
+                connReedSwitch.connect();
+
                 statusWater = connWater.getResponseCode();
-                statusSound = connWater.getResponseCode();
+                statusSound = connSound.getResponseCode();
+                statusGas = connGas.getResponseCode();
+                statusMotion = connMotion.getResponseCode();
+                statusReedSwitch = connReedSwitch.getResponseCode();
+
             } catch (IOException ex) {
                 throw new RuntimeException(ex);
             }
 
             StringBuilder sbWater = null;
             StringBuilder sbSound = null;
-            if (statusWater == 200 && statusSound == 200) {
+            StringBuilder sbGas = null;
+            StringBuilder sbMotion = null;
+            StringBuilder sbReedSwitch = null;
+
+            if (statusWater == 200 && statusSound == 200 && statusGas == 200 && statusMotion == 200 && statusReedSwitch == 200) {
+
                 InputStream isWater = null;
                 InputStream isSound = null;
+                InputStream isGas = null;
+                InputStream isMotion = null;
+                InputStream isReedSwitch = null;
+
                 try {
+
                     isWater = connWater.getInputStream();
                     isSound = connSound.getInputStream();
+                    isGas = connGas.getInputStream();
+                    isMotion = connMotion.getInputStream();
+                    isReedSwitch = connReedSwitch.getInputStream();
+
                 } catch (IOException ex) {
                     throw new RuntimeException(ex);
                 }
+
                 BufferedReader readerWater = new BufferedReader(new
                         InputStreamReader(isWater));
+
                 BufferedReader readerSound = new BufferedReader(new
                         InputStreamReader(isSound));
+
+                BufferedReader readerGas = new BufferedReader(new
+                        InputStreamReader(isGas));
+
+                BufferedReader readerMotion = new BufferedReader(new
+                        InputStreamReader(isMotion));
+
+                BufferedReader readerReedSwitch = new BufferedReader(new
+                        InputStreamReader(isReedSwitch));
+
                 String responseStringWater;
                 String responseStringSound;
+                String responseStringGas;
+                String responseStringMotion;
+                String responseStringReedSwitch;
+
                 sbWater = new StringBuilder();
                 sbSound = new StringBuilder();
+                sbGas = new StringBuilder();
+                sbMotion = new StringBuilder();
+                sbReedSwitch = new StringBuilder();
+
                 while (true) {
+
                     try {
+
                         if ((responseStringWater = readerWater.readLine()) == null)
                             break;
+
                         if ((responseStringSound = readerSound.readLine()) == null)
                             break;
+
+                        if ((responseStringGas = readerGas.readLine()) == null)
+                            break;
+
+                        if ((responseStringMotion = readerMotion.readLine()) == null)
+                            break;
+
+                        if ((responseStringReedSwitch = readerReedSwitch.readLine()) == null)
+                            break;
+
                     } catch (IOException ex) {
                         throw new RuntimeException(ex);
                     }
+
                     sbWater = sbWater.append(responseStringWater);
                     sbSound = sbSound.append(responseStringSound);
+                    sbGas = sbGas.append(responseStringGas);
+                    sbMotion = sbMotion.append(responseStringMotion);
+                    sbReedSwitch = sbReedSwitch.append(responseStringReedSwitch);
+
                 }
             }
+
             assert sbWater != null;
             assert sbSound != null;
+            assert sbGas != null;
+            assert sbMotion != null;
+            assert sbReedSwitch != null;
+
             String valuesWater = sbWater.toString();
             String valuesSound = sbSound.toString();
+            String valuesGas = sbGas.toString();
+            String valuesMotion = sbMotion.toString();
+            String valuesReedSwitch = sbReedSwitch.toString();
+
             Log.d("VALUESWATER", valuesWater);
             Log.d("VALUESSOUND", valuesSound);
+            Log.d("VALUESGAS", valuesGas);
+            Log.d("VALUESMOTION", valuesMotion);
+            Log.d("VALUESREEDSWITCH", valuesReedSwitch);
+
             return (0L);
         }
     }
